8b53bdb0919aa8e599d77f7581987ec7ca348dcc
StanislavaUsenko <StanislavaUsenko@gmail.com>
2019-09-03 21:17:20 +0300

    add project

---
+++ BinarySearch/BinarySearchExtention.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    public class BinarySearchExtention<T>
+    { 
+        public int Search(this T[] array, T searchedValue)
+        {
+            IComparer<T> comparer;
+            if (array == null || searchedValue == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (searchedValue is IComparable)
+            {
+                comparer = Comparer<T>.Default;
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+
+            int left = 0;
+            int right = array.Length - 1;
+            while (left <= right)
+            {
+                var middle = (left + right) / 2;
+                int compare =  comparer.Compare(array[middle], searchedValue);
+
+                if (compare == 0)
+                {
+                    return middle;
+                }
+                else if (compare < 0)
+                {
+                    right = middle - 1;
+                }
+                else
+                {
+                    left = middle + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ FibonachiTask/Fibonachi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonachiTask
+{
+    public class Fibonachi
+    {
+        static IEnumerable<long> FindFibonachi(int countOfNumbers)
+        {
+            long prev = -1;
+            long next = 1;
+            for (int i = 0; i< countOfNumbers; i++)
+            {
+                long sum = prev + next;
+                prev = next;
+                next = sum;
+                yield return sum;
+            }
+        }
+    }
+}
